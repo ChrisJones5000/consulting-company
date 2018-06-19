79ac6a705c5bf6e4710256b6d32238981cae3da8
Chris <chrisjones5000@yahoo.com>
2018-06-19 07:45:22 -0500

    Completed header slider

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import Slider from './modules/Slider';
+// import Slider from './modules/Slider';
+import SlickSlider from './modules/SlickSlider';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.welcome__buttons'), 'slide-left', '80%');
 new RevealOnScroll($('.welcome__blurb, .inline-blurb'), 'reveal-item', '80%');
 // new Slider();
+new SlickSlider($('.slider'));
---
+++ app/assets/scripts/modules/SlickSlider.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+class SlickSlider {
+	constructor(el) {
+		this.slider = el;
+		this.buildSlider();
+	}
+
+	buildSlider() {
+		this.slider.slick({
+			
+		});
+	}
+}
+
+export default SlickSlider;
